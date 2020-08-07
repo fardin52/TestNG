a7a636af55aa8864d6b9c99ca9c87f3f6375f832
fardin52 <fardinna52@gmail.com>
2020-08-07 18:10:08 -0400

    second changes is come

---
+++ again/src/test/java/prcticeRepo/again/toti.java
@@ -0,0 +1,19 @@
+package prcticeRepo.again;
+
+public class toti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("i love kabul");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
