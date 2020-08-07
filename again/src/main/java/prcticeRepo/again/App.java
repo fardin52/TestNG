05b48bdd8bfff54720415d3758bc16bdf83b5a80
fardin52 <fardinna52@gmail.com>
2020-08-07 18:01:10 -0400

    again repo git for practice

---
+++ again/src/main/java/prcticeRepo/again/App.java
@@ -0,0 +1,13 @@
+package prcticeRepo.again;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
